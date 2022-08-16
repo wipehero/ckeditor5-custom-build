cacef0e397d8ef14cfdf7a0e473c9526d4c8f4e4
Yashar <yashar@babrzadeh.com>
2022-08-16 12:29:47 +0430

    init

---
+++ build/translations/tt.js
@@ -0,0 +1,1 @@
+(function(t){const i=t["tt"]=t["tt"]||{};i.dictionary=Object.assign(i.dictionary||{},{"%0 of %1":"",Bold:"Калын",Cancel:"",Italic:"",Redo:"Кабатла","Rich Text Editor. Editing area: %0":"",Save:"Сакла","Show more items":"",Underline:"",Undo:""});i.getPluralForm=function(t){return 0}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
---
+++ src/ckeditor.js
@@ -0,0 +1,50 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import BalloonEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+
+class Editor extends BalloonEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Autoformat,
+	Bold,
+	Essentials,
+	Heading,
+	Italic,
+	Link,
+	List,
+	Paragraph,
+	Underline
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'|',
+			'link',
+			'|',
+			'bulletedList',
+			'numberedList'
+		]
+	},
+	language: 'en'
+};
+
+export default Editor;
